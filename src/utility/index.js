a4aa751b87ef5dc3fa2e4131ecfb596da8d88b22
Jmarsh15 <marshall1515@yahoo.com>
2019-08-13 15:01:54 -0500

    backend sever url

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,4 +1,4 @@
- export const server_url = 'http://9234feac.ngrok.io/'
+ export const server_url = 'http://dev.vala.tv/'
  export const BACKEND_API = 'http://2c29e1ef.ngrok.io'
- export const FRONTEND_URL = 'http://9234feac.ngrok.io/'
+ export const FRONTEND_URL = 'http://dev.vala.tv/'
  export const GOOGLEANALYICS = 'UA-102566873-1'
