21ed53b45a58df0020604f45629942cc373a44b1
Jmarsh15 <marshall1515@yahoo.com>
2019-08-13 15:00:59 -0500

    backend sever url

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,4 +1,4 @@
  export const server_url = 'http://9234feac.ngrok.io/'
- export const BACKEND_API = 'http://f16c6e26.ngrok.io/api/'
+ export const BACKEND_API = 'http://2c29e1ef.ngrok.io'
  export const FRONTEND_URL = 'http://9234feac.ngrok.io/'
  export const GOOGLEANALYICS = 'UA-102566873-1'
